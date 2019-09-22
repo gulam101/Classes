4d77aa7d9706cbde168bdd14f1c7be323592ee2a
unknown <gfram@hotmail.com>
2019-09-22 14:41:15 +0100

    first commit

---
+++ Clases/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Person
+    {
+        public string Name;
+        public string House;
+
+        public void Introduce(string to)
+        {
+            Console.WriteLine("Hi {0}, I am {1} in {2}", to, Name, House);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.Name = "Cat";
+            person.House = "ENG";
+            person.Introduce("Teddy");
+        }
+    }
+}
